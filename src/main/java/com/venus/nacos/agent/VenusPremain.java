dfc52b57e0882d4f11d2daa5d8d2b3d4d14409b0
zhanglei <zhang_lei4@venusgroup.com.cn>
2026-01-19 14:03:41 +0800

    2.5.2

--- src/main/java/com/venus/nacos/agent/VenusPremain.java
+++ src/main/java/com/venus/nacos/agent/VenusPremain.java
@@ -1,12 +1,13 @@
 package com.venus.nacos.agent;
 
 import java.lang.instrument.Instrumentation;
+import java.util.jar.JarFile;
 
 public class VenusPremain {
     public static void premain(String agentArgs, Instrumentation inst) {
         try {
-//            JarFile jarFile = new JarFile(agentArgs);
-//            inst.appendToSystemClassLoaderSearch(jarFile);
+            JarFile jarFile = new JarFile(agentArgs);
+            inst.appendToSystemClassLoaderSearch(jarFile);
             inst.addTransformer(new VenusTransformer(), true);
         } catch (Exception e) {
             throw new RuntimeException(e);
